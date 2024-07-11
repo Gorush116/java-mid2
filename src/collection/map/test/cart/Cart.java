8a6854ef162441e2c9a2623659385bc04178f9f8
Gorush116 <gorush34@gmail.com>
2024-07-11 17:03:48 +0900

    Collection Framework - Stack, Queue, Deque commit

--- src/collection/map/test/cart/Cart.java
+++ src/collection/map/test/cart/Cart.java
@@ -8,23 +8,11 @@ public class Cart {
     private Map<Product, Integer> productMap = new HashMap<>();
 
     public void add(Product product, int quantity) {
-//        if (productMap.containsKey(product)) {
-//            productMap.put(product, productMap.get(product) + quantity);
-//        } else {
-//            productMap.put(product, quantity);
-//        }
         Integer existingQuantity = productMap.getOrDefault(product, 0);
         productMap.put(product, existingQuantity + quantity);
     }
 
     public void minus(Product product, int quantity) {
-//        if (productMap.containsKey(product)) {
-//            if (productMap.get(product) > quantity) {
-//                productMap.put(product, productMap.get(product) - quantity);
-//            } else {
-//                productMap.remove(product);
-//            }
-//        }
         int existingQuantity = productMap.getOrDefault(product, 0);
         int newQuantity = existingQuantity - quantity;
         if (newQuantity <= 0) {
